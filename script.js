293d8370f8125ce6e242c5d6aff31ad7fefd7d2e
Helen-122 <helene.mazourine@gmail.com>
2021-04-06 22:28:52 +0200

    partie 2 JS exo4

---
+++ script.js
@@ -0,0 +1,5 @@
+function displayNone(){
+      var firstName = document.getElementById('firstName').value =' ';
+      var name = document.getElementById('name').value =' ';
+      var city = document.getElementById('city').value =' ';
+  }
